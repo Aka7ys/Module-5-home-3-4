12d3a6823779ee29e9ae99c1ef4cdda1b5eab891
Akarys <Akarys@MACHINE-47>
2023-10-26 10:00:27 +0600

    Добавьте файлы проекта.

---
+++ Module-5-home-3-4/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            Method1();
+        }
+        catch (Exception ex)
+        {
+            // Перехватываем исключение, поднятое из вызываемого метода
+            Console.WriteLine($"Произошла ошибка: {ex.Message}");
+        }
+        finally
+        {
+            // Финальный блок, который всегда будет выполнен
+            Console.WriteLine("Завершение программы.");
+        }
+    }
+
+    static void Method1()
+    {
+        try
+        {
+            // Вызываем метод, который генерирует исключение
+            Method2();
+        }
+        catch (Exception ex)
+        {
+            // Перехватываем исключение и поднимаем его в вызывающий метод
+            throw new Exception($"Ошибка в Method1: {ex.Message}");
+        }
+    }
+
+    static void Method2()
+    {
+        // Генерируем исключение в этом методе
+        throw new Exception("Ошибка в Method2: пример исключения.");
+    }
+}
